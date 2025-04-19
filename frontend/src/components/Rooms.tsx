98a3a041cedd3047cb7d9b1047a5ade169d36af0
chacha <chacha.love.sho@icloud.com>
2025-04-19 16:59:12 +0900

    roomsのエラーを修正

--- frontend/src/components/Rooms.tsx
+++ frontend/src/components/Rooms.tsx
@@ -22,7 +22,7 @@ const Rooms = () => {
   return (
     <>
       <h1 className="text-2xl font-bold">rooms</h1>
-      {isLoading || !data || roomInfos.length === 0 ? (
+      {isLoading || !data || !roomInfos ? (
         <p>loading...</p>
       ) : (
         <>
